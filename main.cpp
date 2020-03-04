8f5ef4a76e5b2f2af4c2d5b289acb1a8ca23d8f8
matkatmusic <matkatmusic@gmail.com>
2020-03-04 13:23:06 -0800

    added part2 instructions

--- main.cpp
+++ main.cpp
@@ -1,66 +1,36 @@
-/*
- Project 3 - Part 1 / 5
-Video:  Chapter 2 Part 5
- User-Defined Types
+ /*
+ Project 3 - Part 2 / 5
+ Video: Chapter 2 Part 6
+ Implementations tasks
+ 
+Create a branch named Part2
 
- Create a branch named Part1
+ tasks
+ 1) write the definition for the Type that leftFoot and rightFoot are instantiations of.
+    don't forget to define and implement the member functions 'stepForward()' and 'stepSize()'
+    you should be able to deduce the return type of those functions based on their usage in Person::run()
+    You'll need to insert the Person struct from the video in the space below.
+ */
+
+
+
+
+
+ /*
+ 2) provide implementations for the member functions you declared in your 10 user-defined types from the previous video outside of your UDT definitions.
  
- 1) write 10 user-defined types, each with a random number of member variables
-    make the member variables have names that are related to type of work the UDT would perform.
-    see the example below for clarity on this instruction
+ 3) be sure to write the correct full qualified name for the nested type's member functions.
  
- 2) give the member variables relevant data types
- 
- 3) add at least 2 member functions to each of your 10 types.  
-    make the function parameter list for those member functions use some of your User-Defined Types
-    You'll write definitions in part2 for these functions
-    you'll call each of these functions in part3
- 
- 4) make 2 of the 10 user-defined types have a nested class.  this nested class also needs member functions.
- 
- 5) One of your 10 UDTs should only use UDTs for its member variable types.   
-     No primitives allowed!
- 
- 6) After you finish defining each type, click the [run] button.  Clear up any errors or warnings as best you can.
+ 4) After you finish defining each type/function, click the [run] button.  Clear up any errors or warnings as best you can.
  
  Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
  
- Make a pull request after you make your first commit and pin the pull request link to our DM thread.
+Make a pull request after you make your first commit and pin the pull request link to our DM thread.
 
 send me a DM to check your pull request
 
  Wait for my code review.
  */
-
-/*
- example:
- */
-//1) a U.D.T. with a random number of member variables
-struct CarWash           
-{
-    //2) member variables with relevant data types.  the names are appropriate for the U.D.T.'s purpose.
-    int numSponges = 3;
-    double amountOfSoapUsedPerCar = 2.6;
-    unsigned int numCarsProcessed = 0;
-    
-    struct Car                             //4) nested class
-    {
-        //2) member variables with relevant data types.  the names are appropriate for the U.D.T.'s purpose.
-        bool isAPickupTruck = false;
-        float gasMileage = 26.2f;        
-
-        //3) a member function whose parameter has a default value.
-        //the parameter name is related to the work the function will perform.
-        void fillTank(double fuelAmountInGallons = 2.0);   
-    };
-    
-    //3) member function with a user-defined type as the parameter.
-    //The user-defined type parameter happens to be the nested class.
-    void washAndWaxCar( Car car );           
-    
-    //5) a member variable whose type is a UDT.
-    Car myCar;  
-};
 
 
 /*
