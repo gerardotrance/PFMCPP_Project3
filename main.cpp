fd1a4fac522dce8c4d52945fc5152404a6a34059
matkatmusic <matkatmusic@gmail.com>
2020-04-19 09:42:46 -0700

    added part4 instructions

--- main.cpp
+++ main.cpp
@@ -1,36 +1,48 @@
+/*
+ Project 3 - Part 4 / 5
+ video: Chapter 2 - Part 9
+ Member initialization tasks
 
- /*
- Project 3 - Part 3 / 5
- video: Chapter 2 - Part 8
- Constructors tasks
-
- Create a branch named Part3
-
- On a new branch:
-
- 1) Add a constructor for each User-Defined-Type.
+ Create a branch named Part4
  
- 2) amend some of your UDT's member functions to print out something interesting via std::cout
- 
- 3) Instantiate 1 or 2 instances of each of your user-defined types in the main function at the bottom of that file,
+ 1) initialize some of your member variables either in-class or in the Constructor member initializer list.
 
- 4) call some of your UDT's amended member functions in main().
+ 2) make some of your member functions use those initialized member variables via std::cout statements.
  
- 5) add some std::cout statements in main() that print out your UDT's member variable values or values returned from your UDT member functions (if they return values)
- 
- After you finish defining each type/function:
- click the [run] button.  Clear up any errors or warnings as best you can.
+ 3) click the [run] button.  Clear up any errors or warnings as best you can.
  
  Commit your changes by clicking on the Source Control panel on the left, entering a message, and click [Commit and push].
  
  Make a pull request after you make your first commit and pin the pull request link to our DM thread.
 
- send me a DM to check your pull request
+send me a DM to check your pull request
 
  Wait for my code review.
- 
- example:
  */
+
+#include <iostream>
+namespace Example {
+struct UDT  
+{
+    int a; //a member variable
+    float b { 2.f }; //3) in-class initialization
+    UDT() : a(0) { } //3) 'constructor-initializer-list' member variable initialization
+    void printThing()  //the member function
+    {
+        std::cout << "UDT::printThing() a:" << a << " b: " << b << std::endl;  //4) printing out something interesting
+    }
+};
+
+int main()
+{
+    UDT foo; //instantiating a Foo in main()
+    foo.printThing(); //calling a member function of the instance that was instantiated.
+    return 0;
+}
+}
+
+//call Example::main()
+
 
 #include <iostream>
 namespace Example 
